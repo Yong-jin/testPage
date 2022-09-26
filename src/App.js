f7aebf556421c66fe7880a6ac5568f6215f81381
ssgH <takeardor@gmail.com>
2022-09-27 00:22:22 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 function App() {
+  let navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={()=>{navigate('/')}}>Home</button>
+        <button onClick={()=>{navigate('/submain')}}>SubMain</button>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/submain/*" element={<SubMain/>}/>
+          {/* <Route path="/detail" element={<Detail id='fromRoot'/>}/> */}
+        </Routes>
       </header>
     </div>
   );
+}
+
+function Home(){
+  return(
+    <div>Home</div>
+  )
+}
+
+function SubMain(){
+  let navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={()=>{navigate('./detail')}}>detail</button>
+      <div>SubMain</div>
+      <Routes>
+        <Route path="/" element={<div>submainInsubmain</div>}/>
+        <Route path=":id" element={<div>submainInsubmain with id</div>}/>
+        <Route path="detail" element={<Detail id='fromSubMain'/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+function Detail({id}){
+  return (
+    <div>
+      Detail {id}
+    </div>
+  )
 }
 
 export default App;
